0d4d49184c3c3e2b94a8a17ae4ad28b0cf7de6a0
Chen <chenyu.zhu86@gmail.com>
2018-09-10 17:08:36 -0400

    Updated RPS game v0.2

--- projects/RPS/rpsjs.js
+++ projects/RPS/rpsjs.js
@@ -5,7 +5,9 @@ let playerScore = 0
 let compScore = 0
 
 function choose(choice) {
+
   playerChoice = choice
+
   if (playerChoice === 'rock') {
     document.getElementById("player-choice").src="rock.png"
   } else if (playerChoice === 'scissors') {
@@ -15,6 +17,7 @@ function choose(choice) {
   }
 
   compRand = Math.floor(Math.random() * 3)
+
   if (compRand === 0){
     compChoice = 'rock'
     document.getElementById("comp-choice").src="rock.png"
@@ -30,17 +33,35 @@ function choose(choice) {
     document.getElementById("results").innerHTML = "It's a tie!"
   } else if (playerChoice === "rock" && compChoice === "paper"){
     document.getElementById("results").innerHTML = "Computer wins!"
+    compScore++
   } else if (playerChoice === "rock" && compChoice === "scissors"){
     document.getElementById("results").innerHTML = "You win!"
+    playerScore++
   } else if (playerChoice === "paper" && compChoice === "scissors"){
     document.getElementById("results").innerHTML = "Computer wins!"
+    compScore++
   } else if (playerChoice === "paper" && compChoice === "rock"){
     document.getElementById("results").innerHTML = "You win!"
+    playerScore++
   } else if (playerChoice === "scissors" && compChoice === "rock"){
     document.getElementById("results").innerHTML = "Computer wins!"
+    compScore++
   } else if (playerChoice === "scissors" && compChoice === "paper"){
     document.getElementById("results").innerHTML = "You win!"
+    playerScore++
   }
+
+  document.getElementById("player-score").innerHTML = playerScore
+  document.getElementById("comp-score").innerHTML = compScore
+
+
+}
+
+function reset () {
+  playerScore = 0
+  compScore = 0
+  document.getElementById("player-score").innerHTML = playerScore
+  document.getElementById("comp-score").innerHTML = compScore
 }
 
 
